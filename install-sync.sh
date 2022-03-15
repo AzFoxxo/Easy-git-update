8c5a3002cbb148878c3eb63649c1e8034fe2bcbc
AzTheFox/KnotMasterAz <63619429+KnotMasterAz@users.noreply.github.com>
2022-03-15 21:17:51 +0000

    Added info

--- install-sync.sh
+++ install-sync.sh
@@ -9,7 +9,7 @@ rm -f tcc.zip
 tcc/tcc/tcc.exe git-update.c -o sync.exe
 
 # Optional extract into project and run for the first time
-mv sync.exe ..
-file=$(pwd)
-cd ..
-rm -rf $file
+mv sync.exe .. # move to root of project
+file=$(pwd) # save directory path
+cd .. # move to root of project
+rm -rf $file # delete repo used to build
