d7bf18049a2c9f653347d298f054d876972c3565
Az <N/A>
2022-03-15 21:13:54 +0000

    Add changes and commit!

--- install-sync.sh
+++ install-sync.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
 
-# wget "https://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27-win64-bin.zip"
-
+# Download, extract and delete
 curl -o tcc.zip "https://mirrors.sarata.com/non-gnu/tinycc/tcc-0.9.27-win64-bin.zip"
 unzip tcc.zip -d "tcc"
 rm -f tcc.zip
 
+# Compile sync
 tcc/tcc/tcc.exe git-update.c -o sync.exe
+
+# Optional extract into project and run for the first time
+mv sync.exe ..
+file=$(pwd)
+cd ..
+rm -rf $file
